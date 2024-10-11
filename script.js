a67c07eaa03234f2a7b9a75b961299e97232cda0
grupo001efg <163312970+grupo001efg@users.noreply.github.com>
2024-10-11 08:40:11 -0300

    script.001

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const votos = {
+    "Free Fire": 0,
+    "Minecraft": 0,
+    "GTA": 0,
+    "Roblox": 0,
+    "Angela": 0
+};
+
+function registrarVoto() {
+    const form = document.getElementById('votacao');
+    const resultado = document.getElementById('resultado');
+    const voto = form.comida.value;
+
+    if (voto) {
+        votos[voto]++;
+        resultado.innerHTML = `Você votou em: ${voto}`;
+        atualizarContador();
+    } else {
+        resultado.innerHTML = 'Por favor, selecione uma opção.';
+    }
+}
+
+function atualizarContador() {
+    document.getElementById('freeFireCount').innerText = votos["Free Fire"];
+    document.getElementById('minecraftCount').innerText = votos["Minecraft"];
+    document.getElementById('gtaCount').innerText = votos["GTA"];
+    document.getElementById('robloxCount').innerText = votos["Roblox"];
+    document.getElementById('angelaCount').innerText = votos["Angela"];
+}
 
